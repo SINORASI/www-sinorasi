c31c610a0049ff36745fe24ab53b50cf5c3076c2
mochraiyan <mra.alfikri@gmail.com>
2025-10-17 20:54:13 +0700

    done

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,8 +1,10 @@
+
+
 import { createAuthClient } from "better-auth/client"
 import { usernameClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000", // Use empty string for relative URLs in Nuxt
+    baseURL: "", // Use empty string for relative URLs in Nuxt
     plugins: [
         usernameClient()
     ]
